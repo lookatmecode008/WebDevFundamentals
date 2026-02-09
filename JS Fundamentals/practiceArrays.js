9db7b45a3557f73220de5427d11e1a1141e67a9f
lookatmecode008 <149254764+lookatmecode008@users.noreply.github.com>
2026-02-09 16:55:23 +0200

    Add files via upload

---
+++ JS Fundamentals/practiceArrays.js
@@ -0,0 +1,39 @@
+const toDoList = [{name: 'wash dishes', date: '05/12/2024'}]
+const toDoInput = document.querySelector('.insertToDo');
+const dueDateInput = document.querySelector('.insertDate');
+
+const savedList = localStorage.getItem('myList');
+let memItem = JSON.parse(savedList)
+
+const addButton = document.querySelector('.add-button');
+addButton.onclick = () => {
+    toDoList.push({name: toDoInput.value, date:dueDateInput.value})
+    toDoInput.value = '';
+    createToDoList()
+}
+    
+ function createToDoList(){ 
+    let outputHTML=``;
+    for (let i = 0; i < toDoList.length; i++) {
+        const name=toDoList[i].name;
+        const date=toDoList[i].date;
+        
+        item =
+        `<p>${name} ${date} 
+            <button 
+                onclick="
+                    toDoList.splice(${i},1);
+                    createToDoList();
+                ">Delete
+            </button>
+        </p>` 
+        
+        localStorage.setItem('myList', JSON.stringify(toDoList));
+        outputHTML += item;
+        document.querySelector('.outputScreen'). innerHTML = outputHTML
+    }
+ }
+
+ setInterval (function () {
+    console.log ('hello')
+ }, 2000)
